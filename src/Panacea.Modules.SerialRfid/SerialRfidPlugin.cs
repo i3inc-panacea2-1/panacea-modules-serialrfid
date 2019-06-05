485a4a19bc2f24fb6abf24b1c328c89c7714e4f9
Portno <j.remoundos@dotbydot.gr>
2019-06-05 18:06:30 +0300

    watcher

--- src/Panacea.Modules.SerialRfid/SerialRfidPlugin.cs
+++ src/Panacea.Modules.SerialRfid/SerialRfidPlugin.cs
@@ -11,21 +11,22 @@ namespace Panacea.Modules.SerialRfid
     public class SerialRfidPlugin : IRfidReaderPlugin
     {
         private SerialPort sp;
-
+        private SerialPortWatcher _watcher;
         public event EventHandler<string> CardConnected;
         public event EventHandler<string> CardDisconnected;
 
         public SerialRfidPlugin()
         {
-            sp = new SerialPort("COM7");
-            sp.DataReceived += (oo, ee) =>
-            {
-                string indata = sp.ReadExisting();
-                var id = indata.Trim(new char[] { '\r', '\n' });
-                CardConnected?.Invoke(this, id);
-                CardDisconnected?.Invoke(this, id);
-            };
+            _watcher = new SerialPortWatcher();
+            _watcher.PortsChanged += _watcher_PortsChanged;
+            
         }
+
+        private void _watcher_PortsChanged(object sender, EventArgs e)
+        {
+            OpenPortIfAvailable();
+        }
+
         public Task BeginInit()
         {
             return Task.CompletedTask;
@@ -33,12 +34,33 @@ namespace Panacea.Modules.SerialRfid
 
         public void Dispose()
         {
-            sp.Dispose();  
+            _watcher?.Dispose();
+            sp?.Dispose();  
+        }
+
+        private void OpenPortIfAvailable()
+        {
+            if (_watcher.ComPorts.Any(p => p == "COM7"))
+            {
+                if(sp != null)
+                {
+                    sp.Dispose();
+                }
+                sp = new SerialPort("COM7");
+                sp.DataReceived += (oo, ee) =>
+                {
+                    string indata = sp.ReadExisting();
+                    var id = indata.Trim(new char[] { '\r', '\n' });
+                    CardConnected?.Invoke(this, id);
+                    CardDisconnected?.Invoke(this, id);
+                };
+                sp.Open();
+            }
         }
 
         public Task EndInit()
         {
-            sp.Open();
+            OpenPortIfAvailable();
             return Task.CompletedTask;
         }
 
